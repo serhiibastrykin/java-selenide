5eeacccb95aed9596db7434482a87a3814c59472
Serhii Bastrykin <sergiy2497@gmail.com>
2021-01-14 16:43:19 +0200

    Create page object and tests for table data downloading and comparison
    * Also remove the last(empty) row inside all classes

--- src/main/java/com/seleniumeasy/enums/Table.java
+++ src/main/java/com/seleniumeasy/enums/Table.java
@@ -4,7 +4,8 @@ public enum Table {
     TABLE_PAGINATION("Table Pagination"),
     TABLE_DATA_SEARCH("Table Data Search"),
     TABLE_FILTER("Table Filter"),
-    TABLE_SORT_AND_SEARCH("Table Sort & Search");
+    TABLE_SORT_AND_SEARCH("Table Sort & Search"),
+    TABLE_DATA_DOWNLOAD("Table Data Download");
 
     private final String val;
 
---
+++ src/main/java/com/seleniumeasy/table/TableDataDownloadPage.java
@@ -0,0 +1,156 @@
+package com.seleniumeasy.table;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.WebDriverRunner;
+import com.seleniumeasy.DemoHomePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static java.util.Objects.requireNonNull;
+import static org.junit.Assert.assertEquals;
+import static utils.SettingsSeleniumEasy.DOWNLOAD_DIR;
+
+public class TableDataDownloadPage extends DemoHomePage {
+    public static final String FILE = "Selenium Easy - Download Table Data to CSV, Excel, PDF and Print.csv";
+    private final SelenideElement inputSearch = $("#example_filter input");
+    private final ElementsCollection headers = $$("thead th"),
+            rows = $$("tbody tr"),
+            buttons = $$(".dt-buttons span"),
+            pagination = $$("#example_paginate a");
+
+    public TableDataDownloadPage clickColumn(int colIndex) {
+        headers.get(colIndex).shouldBe(visible).click();
+        return this;
+    }
+
+    public TableDataDownloadPage clickColumn(String colName) {
+        headers.find(exactText(colName)).shouldBe(visible).click();
+        return this;
+    }
+
+    public TableDataDownloadPage clickButton(int buttonIndex) {
+        buttons.get(buttonIndex).shouldBe(visible).click();
+        return this;
+    }
+
+    public TableDataDownloadPage clickButton(String buttonName) {
+        buttons.find(exactText(buttonName)).shouldBe(visible).click();
+        return this;
+    }
+
+    public TableDataDownloadPage clickPage(int pageIndex) {
+        pagination.get(pageIndex).shouldBe(visible).click();
+        return this;
+    }
+
+    public TableDataDownloadPage enterSearchText(String text) {
+        inputSearch.shouldBe(empty).sendKeys(text);
+        return this;
+    }
+
+    public void validateFileIsDownloaded() throws IOException {
+        File downloadedFile = new File(DOWNLOAD_DIR + File.separator + FILE);
+        waitForFile(downloadedFile);
+        if (!downloadedFile.exists()) {
+            throw new IOException("The file doesn't exist!");
+        }
+    }
+
+    public void closePrintDialog() {
+        try {
+            Robot r = new Robot();
+            r.keyPress(KeyEvent.VK_ESCAPE);
+            r.keyRelease(KeyEvent.VK_ESCAPE);
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void compareDisplayedAndDownloadedData() throws IOException {
+        List<List<String>> entireContent = getEntireContent();
+        List<String> joinedContent = joinContent(entireContent);
+        assertEquals(joinedContent, readDownloadedFileContent());
+    }
+
+    private void waitForFile(File file) {
+        Wait<WebDriver> wait = new FluentWait<>(WebDriverRunner.getWebDriver())
+                .withTimeout(Duration.ofSeconds(5))
+                .pollingEvery(Duration.ofMillis(500));
+        wait.until((condition) -> file.exists());
+    }
+
+    private List<String> joinContent(List<List<String>> collections) {
+        String delimiter = "\"";
+        List<String> content = new ArrayList<>();
+        for (List<String> collection : collections) {
+            content.add(delimiter + String.join(delimiter + "," + delimiter, collection) + delimiter);
+        }
+        System.out.println("Number of rows displayed: " + content.size());
+        System.out.println(content);
+        return content;
+    }
+
+    private List<String> readDownloadedFileContent() throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(DOWNLOAD_DIR +
+                File.separator + FILE))) {
+            List<String> fileContent = new ArrayList<>();
+            String strCurrentLine;
+            while ((strCurrentLine = reader.readLine()) != null) {
+                fileContent.add(strCurrentLine);
+            }
+            reader.close();
+            System.out.println("Number of rows inside the file: " + fileContent.size());
+            System.out.println(fileContent);
+            return fileContent;
+        }
+    }
+
+    private List<List<String>> getEntireContent() {
+        List<List<String>> entireContent = new ArrayList<>();
+        entireContent.add(getColumnNames());
+        int i = 1;
+        do {
+            entireContent.addAll(getPageContent());
+            pagination.last().click();
+            i++;
+        } while (requireNonNull(pagination.get(i).getAttribute("class")).contains("current"));
+        return entireContent;
+    }
+
+    private List<List<String>> getPageContent() {
+        List<List<String>> pageContent = new ArrayList<>();
+        for (SelenideElement element : rows) {
+            List<SelenideElement> cells = element.$$(By.tagName("td"));
+            List<String> columns = new ArrayList<>();
+            for (SelenideElement cell : cells) {
+                columns.add(cell.getText());
+            }
+            pageContent.add(columns);
+        }
+        return pageContent;
+    }
+
+    private List<String> getColumnNames() {
+        List<String> colNames = new ArrayList<>();
+        for (SelenideElement header : headers) {
+            colNames.add(header.getText());
+        }
+        return colNames;
+    }
+}
