f9b5af71ea309b6f80a4a855dc628350efb18633
Serhii Bastrykin <sergiy2497@gmail.com>
2021-01-19 09:55:55 +0200

    Add test for entering name in the alert box

--- src/main/java/com/seleniumeasy/alertsandmodals/JavascriptAlertsPage.java
+++ src/main/java/com/seleniumeasy/alertsandmodals/JavascriptAlertsPage.java
@@ -1,15 +1,17 @@
 package com.seleniumeasy.alertsandmodals;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import com.seleniumeasy.DemoHomePage;
 
-import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.switchTo;
 
 public class JavascriptAlertsPage extends DemoHomePage {
     private final SelenideElement buttonClickMe = $("button[onclick='myConfirmFunction()']"),
-            displayedMessage = $("#confirm-demo");
+            displayedMessage = $("#confirm-demo"),
+            buttonClickForPromptBox = $("button[onclick='myPromptFunction()']"),
+            displayedName = $("#prompt-demo");
 
     public JavascriptAlertsPage clickClickMe() {
         buttonClickMe.click();
@@ -26,7 +28,21 @@ public class JavascriptAlertsPage extends DemoHomePage {
         return this;
     }
 
-    public void validateDisplayedMessage(String message) {
-        displayedMessage.shouldHave(exactText(message));
+    public void validateDisplayedMessage(Condition c) {
+        displayedMessage.should(c);
+    }
+
+    public JavascriptAlertsPage clickClickForPromptBox() {
+        buttonClickForPromptBox.click();
+        return this;
+    }
+
+    public JavascriptAlertsPage enterName(String name) {
+        switchTo().alert().sendKeys(name);
+        return this;
+    }
+
+    public void validateDisplayedName(Condition c) {
+        displayedName.should(c);
     }
 }
--- src/test/java/com/seleniumeasy/tests/alertsandmodals/JavascriptAlertsTest.java
+++ src/test/java/com/seleniumeasy/tests/alertsandmodals/JavascriptAlertsTest.java
@@ -4,11 +4,14 @@ import com.seleniumeasy.alertsandmodals.JavascriptAlertsPage;
 import org.junit.Test;
 import utils.SettingsSeleniumEasy;
 
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
 import static com.seleniumeasy.enums.AlertsAndModals.JAVASCRIPT_ALERTS;
 
 public class JavascriptAlertsTest extends SettingsSeleniumEasy {
     private final String confirmMessage = "You pressed OK!",
-            dismissMessage = "You pressed Cancel!";
+            dismissMessage = "You pressed Cancel!",
+            name = "Serhii";
 
     JavascriptAlertsPage javascriptAlertsPage = new JavascriptAlertsPage();
 
@@ -18,7 +21,7 @@ public class JavascriptAlertsTest extends SettingsSeleniumEasy {
                 .openAlertsAndModals(JAVASCRIPT_ALERTS, JavascriptAlertsPage.class)
                 .clickClickMe()
                 .confirmAlert()
-                .validateDisplayedMessage(confirmMessage);
+                .validateDisplayedMessage(exactText(confirmMessage));
     }
 
     @Test
@@ -27,6 +30,16 @@ public class JavascriptAlertsTest extends SettingsSeleniumEasy {
                 .openAlertsAndModals(JAVASCRIPT_ALERTS, JavascriptAlertsPage.class)
                 .clickClickMe()
                 .dismissAlert()
-                .validateDisplayedMessage(dismissMessage);
+                .validateDisplayedMessage(exactText(dismissMessage));
+    }
+
+    @Test
+    public void testEnterNameInAlertBox() {
+        javascriptAlertsPage
+                .openAlertsAndModals(JAVASCRIPT_ALERTS, JavascriptAlertsPage.class)
+                .clickClickForPromptBox()
+                .enterName(name)
+                .confirmAlert()
+                .validateDisplayedName(text(name));
     }
 }
