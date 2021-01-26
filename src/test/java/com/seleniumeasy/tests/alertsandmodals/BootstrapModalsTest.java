edafdfe2c29fd98391248f08af97b177fbc3e357
Serhii Bastrykin <sergiy2497@gmail.com>
2021-01-26 18:32:10 +0200

    Create page object and tests for bootstrap modals

---
+++ src/test/java/com/seleniumeasy/tests/alertsandmodals/BootstrapModalsTest.java
@@ -0,0 +1,43 @@
+package com.seleniumeasy.tests.alertsandmodals;
+
+import com.seleniumeasy.alertsandmodals.BootstrapModalsPage;
+import org.testng.annotations.Test;
+import utils.SettingsSeleniumEasy;
+
+import static com.seleniumeasy.enums.AlertsAndModals.BOOTSTRAP_MODALS;
+
+public class BootstrapModalsTest extends SettingsSeleniumEasy {
+
+    BootstrapModalsPage bootstrapModalsPage = new BootstrapModalsPage();
+
+    @Test
+    public void testClosingMultipleModals() {
+        bootstrapModalsPage
+                .openAlertsAndModals(BOOTSTRAP_MODALS, BootstrapModalsPage.class)
+                .clickLaunchFirstModal()
+                .clickLaunchSecondModal()
+                .closeSecondModal()
+                .verifySecondModalClosed()
+                .closeFirstModal()
+                .verifyFirstModalClosed();
+    }
+
+    @Test
+    public void testSavingChangesInFirstModal() {
+        bootstrapModalsPage
+                .openAlertsAndModals(BOOTSTRAP_MODALS, BootstrapModalsPage.class)
+                .clickLaunchFirstModal()
+                .clickSaveChangesFirstModal()
+                .verifyBothModalsNotDisplayed();
+    }
+
+    @Test
+    public void testSavingChangesInSecondModal() {
+        bootstrapModalsPage
+                .openAlertsAndModals(BOOTSTRAP_MODALS, BootstrapModalsPage.class)
+                .clickLaunchFirstModal()
+                .clickLaunchSecondModal()
+                .clickSaveChangesSecondModal()
+                .verifyBothModalsNotDisplayed();
+    }
+}
