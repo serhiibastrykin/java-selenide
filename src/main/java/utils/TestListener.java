362380bc03a252194fbf2b8e8c029f12579209bb
sbast <sergiy2497@gmail.com>
2021-02-05 17:49:05 +0200

    Implement realization for all ITestListener methods

--- src/main/java/utils/TestListener.java
+++ src/main/java/utils/TestListener.java
@@ -5,38 +5,41 @@ import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 public class TestListener implements ITestListener {
-    @Override
-    public void onTestStart(ITestResult iTestResult) {
 
+    @Override
+    public void onTestStart(ITestResult result) {
+        System.out.println("Test Started " + result.getName());
     }
 
     @Override
-    public void onTestSuccess(ITestResult iTestResult) {
-
+    public void onTestSuccess(ITestResult result) {
+        System.out.println("Test Successfully Finished " + result.getName());
     }
 
     @Override
-    public void onTestFailure(ITestResult iTestResult) {
-
+    public void onTestFailure(ITestResult result) {
+        System.out.println("Test Failed " + result.getName());
     }
 
     @Override
-    public void onTestSkipped(ITestResult iTestResult) {
-
+    public void onTestSkipped(ITestResult result) {
+        System.out.println("Test Skipped " + result.getName());
     }
 
     @Override
-    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
-
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+        System.out.println("Test Failed but within success percentage " + result.getName());
     }
 
     @Override
-    public void onStart(ITestContext iTestContext) {
-        System.out.println(iTestContext.getName());
+    public void onStart(ITestContext context) {
+        System.out.println("First test Started " + context.getName());
     }
 
     @Override
-    public void onFinish(ITestContext iTestContext) {
-
+    public void onFinish(ITestContext context) {
+        System.out.println("Last test Finished " + context.getName());
+        System.out.println("Success Tests " + context.getPassedTests());
+        System.out.println("Failed Tests " + context.getFailedTests());
     }
 }
