6306fcb2111678d2d6791f31864263b018e83a0e
Serhii Bastrykin <sergiy2497@gmail.com>
2021-02-07 13:06:18 +0200

    Create enum for file types and add preference to Firefox config

--- src/main/java/utils/selenoid/SelenoidFirefox.java
+++ src/main/java/utils/selenoid/SelenoidFirefox.java
@@ -11,6 +11,7 @@ import javax.annotation.Nonnull;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import static com.seleniumeasy.enums.FileTypes.*;
 import static utils.SettingsSeleniumEasy.DOWNLOAD_DIR;
 import static utils.SettingsSeleniumEasy.selenoid_URL;
 
@@ -24,8 +25,12 @@ public class SelenoidFirefox implements WebDriverProvider {
         profile.setPreference("browser.download.manager.showWhenStarting", false);
         profile.setPreference("browser.download.dir", DOWNLOAD_DIR);
         profile.setPreference("browser.download.useDownloadDir", DOWNLOAD_DIR);
-        profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "text/csv/xls/xlsx/json/pdf");
-        profile.setPreference("browser.helperApps.neverAsk.openFile", "text/csv/xls/xlsx/json/pdf");
+        String fileTypes = TXT.getType() + ", " + BINARY.getType() + ", "
+                + PDF.getType() + ", " + CSV.getType() + ", " + JPEG.getType() + ", "
+                + XML.getType() + ", " + EXCEL.getType() + ", " + PNG.getType() + ", "
+                + JSON.getType() + ", " + DOCX.getType();
+        profile.setPreference("browser.helperApps.neverAsk.saveToDisk", fileTypes);
+        profile.setPreference("browser.helperApps.neverAsk.openFile", fileTypes);
         profile.setPreference("browser.helperApps.alwaysAsk.force", false);
         profile.setPreference("security.mixed_content.block_active_content", false);
         profile.setPreference("security.mixed_content.block_display_content", false);
